4ff05b03d9cae45b03ad690a5c0207dc228cf29c
Pisitpong122 <73678469+Pisitpong122@users.noreply.github.com>
2020-10-30 16:02:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ส่งงานครับ</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>ส่งงานครับ</h1>   
+        <p>พิสิฐพงศ์ ปาละวิสุทธิ์ (ภู)</p>
+        <a href="https://azure.microsoft.com/en-us/">ล็อคอินไม่ได้ครับ ขอส่งแค่ git ก่อนนะ</a>
+        <br>
+        <img src="https://www.poppaganda.net/wp-content/uploads/2015/06/luhu_sad_cat_02.jpg" width="400" height="300"> 
+        <p>เข้า Microsoft Azure ไม่ได้ครับ นึกว่าจำรหัสผ่านไม่ได้เลยไปรีเซ็ตรหัสผ่าน<br>สรุปกลับมาก็เข้าไม่ได้เหมือนเดิม<br>ขอบคุณครับ</p>
+        <img src="https://sites.google.com/site/funnycatmeawww/_/rsrc/1422326075261/home/6997052-funny-cat.jpg?height=675&width=1200" width="400" height="300">
+    </body>
+</html>
