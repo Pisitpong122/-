de123db2597479adf071ba736c742272e0b38b59
Pisitpong122 <73678469+Pisitpong122@users.noreply.github.com>
2020-11-06 10:56:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>ส่งงานครับ</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <h1>ส่งงานครับ</h1>   
-        <p>พิสิฐพงศ์ ปาละวิสุทธิ์ (ภู)</p>
-        <a href="https://azure.microsoft.com/en-us/">ล็อคอินไม่ได้ครับ ขอส่งแค่ git ก่อนนะ</a>
-        <br>
-        <img src="https://www.poppaganda.net/wp-content/uploads/2015/06/luhu_sad_cat_02.jpg" width="400" height="300"> 
-        <p>เข้า Microsoft Azure ไม่ได้ครับ นึกว่าจำรหัสผ่านไม่ได้เลยไปรีเซ็ตรหัสผ่าน<br>สรุปกลับมาก็เข้าไม่ได้เหมือนเดิม<br>ขอบคุณครับ</p>
-        <img src="https://sites.google.com/site/funnycatmeawww/_/rsrc/1422326075261/home/6997052-funny-cat.jpg?height=675&width=1200" width="400" height="300">
-    </body>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="img/work/work1.jpg" style="height: 100%; width: 100%;" class="img-fluid" />
+            </div>
+            <div class="col-sm-3">
+                <img src="img/work/work2.jpg" class="img-fluid" />
+                <img src="img/work/work4.jpg" class="img-fluid" />
+                <img src="img/work/work6.jpg" class="img-fluid" />
+            </div>
+            <div class="col-sm-3">
+                <img src="img/creative/creative4.jpg" style="width: 100%; height: 86%;" class="img-fluid" />
+                <img src="img/banner/banner2.jpg" class="img-fluid">
+            </div>
+        </div> <!-- Close Row 2 -->
+    </div> <!-- Close container -->
+</body>
+
 </html>
